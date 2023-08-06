e93adec4207fa0a6f9062d39f742be4b30472bb7
glitton <glcodeworks@gmail.com>
2023-08-06 16:21:33 -0700

    render tour component, loading and tours

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    const fetchTours = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        // if (!response.ok) {
+        //   setIsLoading(false);
+        //   return;
+        // }
+        const tours = await response.json();
+        setTours(tours);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  // TO DO:Refresh button
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 };
 export default App;
---
+++ src/components/Tour.jsx
@@ -0,0 +1,4 @@
+const Tour = ({ tour }) => {
+  return <div>Tour</div>;
+};
+export default Tour;
