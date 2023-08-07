041885e4c3ce3732bcd3b8968a060f71143268a7
glitton <glcodeworks@gmail.com>
2023-08-06 19:39:11 -0700

    read more show less functionality done

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  // substring method
+  // console.log(info.substring(0, 40));
   return (
     <article className='single-tour'>
       <img src={image} alt={name} className='img' />
       <span className='tour-price'>${price}</span>
       <div className='tour-info'>
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+        <button
+          type='button'
+          className='info-btn'
+          onClick={() => setReadMore(!readMore)}
+        >
+          {readMore ? "show less" : "read more"}
+        </button>
         <button
           type='button'
           className='btn btn-block delete-btn'
